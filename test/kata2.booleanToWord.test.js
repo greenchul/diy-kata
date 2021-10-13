f9fe0dce167d318b2bd27bfa4196a97992caadea
Rachel Sanham <green_chul@hotmail.com>
2021-10-13 18:33:29 +0100

    First two tests complete

--- test/kata2.booleanToWord.test.js
+++ test/kata2.booleanToWord.test.js
@@ -1,5 +1,11 @@
 const { booleanToWord } = require("../src");
 
 describe("booleanToWord", () => {
-  // how do we create specs again???
+  it("Should return the string yes when passed true",()=>{
+    expect(booleanToWord(true)).toBe('Yes')
+  })
+  it("Should return the string no when passed false", ()=>{
+    expect(booleanToWord(false)).toBe("No")
+  })
+
 });
